332b6610904af99ca340bea5b017750e77aecc6e
HIpex <117768734+Hipex123@users.noreply.github.com>
2024-05-27 14:03:07 +0200

    Update distro50.bat

--- bin/distro50.bat
+++ bin/distro50.bat
@@ -1,52 +1,4 @@
 @echo off
-::IF "%1" == "-r" cmd /k "python main.py -r & set EXITCODE=%ERRORLEVEL% & cd %origin%" IF %EXITCODE% == 1 cmd /k "cd %~dp0..\src & python main.py -r & cd %origin%"
-set "origin=%cd%"
-cd %~dp0..\src
-
-IF "%1" == "" cmd /k "python main.py -h & cd %origin%"
-
-IF "%1" == "--share" IF "%2" =="--help" cmd /k "python main.py -s & cd %origin%"
-IF "%1" == "--help" IF "%2" =="--share" cmd /k "python main.py -s & cd %origin%"
-
-IF "%1" == "--run" IF "%2" =="--share" cmd /k "python main.py -rs & cd %origin%"
-IF "%1" == "--share" IF "%2" =="--run" cmd /k "python main.py -sr & cd %origin%"
-
-IF "%1" == "--help" IF "%2" =="--run" cmd /k "python main.py -r & cd %origin%"
-IF "%1" == "--run" IF "%2" =="--help" cmd /k "python main.py -r & cd %origin%"
-
-
-IF "%1" == "-s" IF "%2" =="-h" cmd /k "python main.py -s & cd %origin%"
-IF "%1" == "-h" IF "%2" =="-s" cmd /k "python main.py -s & cd %origin%"
-
-IF "%1" == "-r" IF "%2" =="-s" cmd /k "python main.py -rs & cd %origin%"
-IF "%1" == "-s" IF "%2" =="-r" cmd /k "python main.py -sr & cd %origin%"
-
-IF "%1" == "-h" IF "%2" =="-r" cmd /k "python main.py -r & cd %origin%"
-IF "%1" == "-r" IF "%2" =="-h" cmd /k "python main.py -r & cd %origin%"
-
-
-IF "%1" == "-s" cmd /k "python main.py -s & cd %origin%"
-IF "%1" == "-h" cmd /k "python main.py -h & cd %origin%"
-IF "%1" == "-r" cmd /k "python main.py -r & cd %origin%"
-
-IF "%1" == "--share" cmd /k "python main.py -s & cd %origin%"
-IF "%1" == "--help" cmd /k "python main.py -h & cd %origin%"
-IF "%1" == "--run" cmd /k "python main.py -r & cd %origin%"
-
-
-IF "%1" == "-rh" cmd /k "python main.py -r & cd %origin%"
-IF "%1" == "-hr" cmd /k "python main.py -r & cd %origin%"
-
-IF "%1" == "-sr" cmd /k "python main.py -sr & cd %origin%"
-IF "%1" == "-rs" (
-    cmd /k "python main.py -rs & cd %origin%"
-) ELSE (
-    cmd /k "python main.py -h & cd %origin%"
-)
-
-
-
-@echo off
 
 set "origin=%cd%"
 cd %~dp0..\src
@@ -55,4 +7,4 @@ set EXITCODE=0
 
 cmd /k "python main.py %1 %2 %3 & set EXITCODE=%ERRORLEVEL% & cd %origin%"
 
-IF %EXITCODE% == 1 cmd /k "cd %~dp0..\src & python main.py %1 %2 %3 & cd %origin%"
+IF %EXITCODE% == 1337 cmd /k "cd %~dp0..\src & python main.py %1 %2 %3 & cd %origin%"
