24e18d5e119d5e9c66ac037c3bbd5af18c35d760
HIpex <117768734+Hipex123@users.noreply.github.com>
2024-05-31 07:59:01 +0200

    Update distro50.bat

--- bin/distro50.bat
+++ bin/distro50.bat
@@ -2,32 +2,10 @@
 
 set "origin=%cd%"
 
-where powershell >nul 2>&1
-if %errorlevel% neq 0 (
-    echo PowerShell is not available. Cannot proceed with installation.
-    exit /b %errorlevel%
-)
-
-py --version > nul
+py --version >nul
 if %ERRORLEVEL% neq 0 (
-    echo Python not found.
-    set /p "userInputO=Would you like to install python 3.10.11 (y/n):"
-    
-    if "%userinputO%"==y (
-        powershell -Command "Invoke-WebRequest -Uri https://www.python.org/ftp/python/3.10.11/python-3.10.11-amd64.exe -OutFile python-installer.exe"
-
-        if not exist python-installer.exe (
-            echo Failed to download Python installer.
-            exit /b 1
-        )
-        echo Installing Python...
-        python-installer.exe /quiet InstallAllUsers=1 PrependPath=1
-    ) else if "%userinputO%"==n (
-        exit
-    ) else (
-        echo Option not recognised. Exiting...
-        exit
-    )
+    echo Python not found
+    echo Go on https://www.python.org/downloads/ and download newer version of python (3.10.11 or higher)
 )
 
 for /f "tokens=2" %%G in ('py --version 2^>^&1') do (
@@ -35,46 +13,30 @@ for /f "tokens=2" %%G in ('py --version 2^>^&1') do (
 )
 
 if %pythonVersion% lss 3.10.11 (
-    echo Python is not up to date.
-    set /p "userInputT=Would you like to install python 3.10.11 (y/n):"
-
-    if "%userInputT%"==y (
-        powershell -Command "Invoke-WebRequest -Uri https://www.python.org/ftp/python/3.10.11/python-3.10.11-amd64.exe -OutFile python-installer.exe"
-
-        if not exist python-installer.exe (
-            echo Failed to download Python installer.
-            exit /b 1
-        )
-        echo Installing Python...
-        python-installer.exe /quiet InstallAllUsers=1 PrependPath=1
-    ) else if "%userInputT%"==n (
-        echo
-    ) else (
-        echo Option not recognised. Exiting...
-        exit
-    )
+    echo Python is not up to date
+    echo Go on https://www.python.org/downloads/ and download newer version of python (3.10.11 or higher)
 )
 
-py --version > nul
+py --version >nul
 if %ERRORLEVEL% neq 0 (
     echo Python installation failed.
     exit /b %errorlevel%
 )
 
-pip --version > nul
+pip --version >nul
 if %ERRORLEVEL% neq 0 (
     echo Pip not found.
-    set /p "userInputTh=Would you like to install pip 24.0 (y/n):"
+    set /p "userInputTh=Would you like to install pip (y/n):"
+)
 
-    if "%userInputTh%"==y (
-        echo Installing pip...
-        python -m pip install pip==24.0
-    ) else if "%userInputTh%"==n (
-        exit
-    ) else (
-        echo Option not recognised. Exiting...
-        exit
-    )
+if %userInputTh%==y (
+    echo Installing pip...
+    py -3 -m ensurepip
+) else if %userInputTh%00n (
+    exit
+) else (
+    echo Option not recognised. Exiting...
+    exit
 )
 
 for /f "tokens=*" %%i in ('pip --version') do (
@@ -88,11 +50,11 @@ for /f "tokens=2 delims= " %%a in ("%pip_version%") do (
 
 if "%pipVersion%" lss "24.0" (
     echo Pip may not be compatible.
-    set /p "userInputF=Would you like to install pip 24.0 (y/n):"
+    set /p "userInputF=Would you like to install pip (y/n):"
 
     if "%userInputF%"==y (
         echo Upgrading pip...
-        python -m pip install pip==24.0
+        py -3 -m ensurepip
     ) else if "%userInputF%"==n (
         echo
     ) else (
