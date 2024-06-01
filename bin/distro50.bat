6ea1e7eabe1dbd2b881eccd6458ea2a2382588a6
HIpex <117768734+Hipex123@users.noreply.github.com>
2024-06-01 14:53:21 +0200

    Update distro50.bat

--- bin/distro50.bat
+++ bin/distro50.bat
@@ -1,12 +1,25 @@
 @echo off
 
-python -m pip install --upgrade pip
+python -m pip install --upgrade pip >nul
 
 set "origin=%cd%"
 
 cd %~dp0..
 
-pip install -r requirements.txt
+pip show gradio >nul
+if %errorlevel% neq 0 (
+    pip install -r requirements.txt
+)
+
+pip show qrcode >nul
+if %errorlevel% neq 0 (
+    pip install -r requirements.txt
+)
+
+pip show pillow >nul
+if %errorlevel% neq 0 (
+    pip install -r requirements.txt
+)
 
 cd src
 
