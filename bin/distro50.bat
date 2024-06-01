c2396fd788fe39685e9e36697d5ea108938bc271
HIpex <117768734+Hipex123@users.noreply.github.com>
2024-06-01 14:38:22 +0200

    Update distro50.bat

--- bin/distro50.bat
+++ bin/distro50.bat
@@ -1,11 +1,13 @@
 @echo off
 
 python -m pip install --upgrade pip
-pip install -r requirements.txt
 
 set "origin=%cd%"
 
 cd %~dp0..
+
+pip install -r requirements.txt
+
 cd src
 
 cmd /k "python main.py %1 %2 %3 & cd %origin%"
